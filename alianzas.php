d6ed36536e254a4aa110d9a031e2c32cce16cf19
codingghosta <sistema@codingghost.com.mx>
2019-02-04 14:46:28 -0600

    Alianzas
    Cuevas

---
+++ alianzas.php
@@ -0,0 +1,353 @@
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Seguimiento Digital | ALIANZAS</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- favicon -->		
+		<link rel="shortcut icon" type="image/x-icon" href="img/logo/favicon.png">
+
+		<!-- all css here -->
+
+		<!-- bootstrap v3.3.6 css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- owl.carousel css -->
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/owl.transitions.css">
+
+		<!-- font-awesome css -->
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/icon.css">
+		<link rel="stylesheet" href="css/flaticon.css">
+        <!-- magnific css -->
+        <link rel="stylesheet" href="css/zebra_datepicker.css">
+		<!-- magnific css -->
+        <link rel="stylesheet" href="css/magnific.min.css">
+		<!-- venobox css -->
+		<link rel="stylesheet" href="css/venobox.css">
+		<!-- style css -->
+		<link rel="stylesheet" href="css/style.css">
+		<!-- responsive css -->
+		<link rel="stylesheet" href="css/responsive.css">
+
+		<!-- modernizr css -->
+		<script src="js/marco/modernizr-2.8.3.min.js"></script>
+        <style type="text/css">
+            .calendario:hover{
+              box-shadow: 0 8px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+              cursor: pointer;
+            }
+        </style>
+	</head>
+		<body>
+
+		<!--[if lt IE 8]>
+			<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<![endif]-->
+
+        <div id="preloader"></div>
+        <!-- header -->
+        <?php 
+            include("mainMenu.php");
+        ?>
+      <section class="page-breadcrumb">
+         <div class="page-section-restaurant">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12 col-sm-12 col-xs-12">
+                     <div class="breadcrumb text-center">
+                        <div class="section-titleBar white-headline text-center">
+                           <h3>ALIANZAS</h3>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="nav-path">
+            <div class="container">
+               <div class="row">
+                  <ul>
+                     <li class="home-bread">INICIO</li>
+                     <li>ALIANZAS</li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- End Slider section -->
+
+        <section class="offer-section sec-padding">
+            <div class="container">
+                <div class="row col-md-12" style="height: 250px; margin-bottom: 25px;">
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      <a  target="_blank" href="https://www.luistopete.com"><img src="img/alianza/lt.png" class="img-responsive calendario" alt="Luis Topete" style="border: #0FB7BF solid; border-width: 0px 5px 5px 0px;"></a>
+                  </div>
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px; text-align: center;">
+                      <span style="color: #006B8F; font-size: 40px; font-weight: 700;">ALIANZAS nacionales<br />e internacionales</span>
+                  </div>
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      <a target="_blank" href="http://www.provantageworld.com/"><img src="img/alianza/pv.png" class="img-responsive calendario" alt="Provantage World" style="border: #0FB7BF solid; border-width: 0px 0px 5px 5px;"></a>
+                  </div>  
+                </div>
+                <div class="row col-md-12" style="height: 250px; margin-bottom: 25px;">
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      
+                  </div>
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      <a target="_blank" href="http://www.ccade.com.mx"><img src="img/alianza/cc.png" class="img-responsive calendario" alt="CCADE" style="border:#0FB7BF solid 5px;"></a>
+                  </div>
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                     
+                  </div>  
+                </div>
+                <div class="row col-md-12" style="height: 250px; margin-bottom: 25px;">
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      <a target="_blank" href="http://www.exitoempresarial.com.mx"><img src="img/alianza/ee.png" class="img-responsive calendario" alt="Éxito Empresarial" style="border: #0FB7BF solid;
+    border-width: 5px 5px 0px 0px;"></a>
+                  </div>
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      <a target="_blank" href="http://www.ebsm.com.mx"><img src="img/alianza/eb.png" class="img-responsive calendario" alt="EBSM"></a>
+                  </div>
+                  <div class="col-md-4 col-sm-12 col-xs-12 col-lg-4 col-xl-4" style="padding-bottom: 5px;">
+                      <a target="_blank" href="http://www.bitworkelite.com"><img src="img/alianza/bi.png" class="img-responsive calendario" alt="BIT" style="border: #0FB7BF solid;
+    border-width: 5px 0px 0px 5px;"></a>
+                  </div>  
+                </div>
+            </div>
+        </section>        
+        <!-- header end -->
+        <!-- Main menu -->
+        <!-- MAin menu ends -->
+        <!-- Start breadcumb section -->
+        <!-- <section class="page-breadcrumb">
+            <div class="page-section">
+                <div class="breadcumb-overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="breadcrumb text-center">
+                                <div class="section-titleBar white-headline text-center">
+                                    <h3>Offers & Deals</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="nav-path">
+                <div class="container">
+                    <div class="row">
+                        <ul>
+                            <li class="home-bread">INICIO</li>
+                            <li>CALENDARIO</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <!-- End breadcumb section -->
+
+        <!-- Start single room section -->
+        <!-- <section class="offer-section sec-padding">
+            <div class="container">
+                <div class="row">
+                   <div class="col-md-6 col-sm-6">
+                       <div class="tour">
+                           <a href="tour-place.html" class="tour-img">
+                               <img src="img/tours/tour-1.jpg" alt="tour-img">
+                               <p class="price">
+                                   <span>$1500</span>
+                                   <small>/ 3 Days</small>
+                               </p>
+                           </a>
+                           <div class="desc">
+                               <h4>Family Tour in Greece</h4>
+                               <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
+                               <div class="rating">
+                                   <i class="fa fa-star voted"></i>
+                                   <i class="fa fa-star voted"></i>
+                                   <i class="fa fa-star voted"></i>
+                                   <i class="fa fa-star"></i>
+                                   <i class="fa fa-star"></i>
+                                   <small>(145)</small>
+                               </div>
+                                    <a class="thm-btn" href="#">BOOK NOW</a>
+                           </div>
+                       </div>
+                   </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="tour">
+                            <a href="tour-place.html" class="tour-img">
+                                <img src="img/tours/tour-2.jpg" alt="tour-img">
+                                <p class="price">
+                                    <span>$1500</span>
+                                    <small>/ 3 Days</small>
+                                </p>
+                            </a>
+                            <div class="desc">
+                                <h4>Family Tour in Greece</h4>
+                                <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
+                                <div class="rating">
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <small>(145)</small>
+                                </div>
+                                <a class="thm-btn" href="#">BOOK NOW</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="tour">
+                            <a href="tour-place.html" class="tour-img">
+                                <img src="img/tours/tour-3.jpg" alt="tour-img">
+                                <p class="price">
+                                    <span>$1500</span>
+                                    <small>/ 3 Days</small>
+                                </p>
+                            </a>
+                            <div class="desc">
+                                <h4>Family Tour in Greece</h4>
+                                <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
+                                <div class="rating">
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <small>(145)</small>
+                                </div>
+                                <a class="thm-btn" href="#">BOOK NOW</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="tour">
+                            <a href="tour-place.html" class="tour-img">
+                                <img src="img/tours/tour-4.jpg" alt="tour-img">
+                                <p class="price">
+                                    <span>$1500</span>
+                                    <small>/ 3 Days</small>
+                                </p>
+                            </a>
+                            <div class="desc">
+                                <h4>Family Tour in Greece</h4>
+                                <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
+                                <div class="rating">
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <small>(145)</small>
+                                </div>
+                                <a class="thm-btn" href="#">BOOK NOW</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="tour">
+                            <a href="tour-place.html" class="tour-img">
+                                <img src="img/tours/tour-1.jpg" alt="tour-img">
+                                <p class="price">
+                                    <span>$1500</span>
+                                    <small>/ 3 Days</small>
+                                </p>
+                            </a>
+                            <div class="desc">
+                                <h4>Family Tour in Greece</h4>
+                                <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
+                                <div class="rating">
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <small>(145)</small>
+                                </div>
+                                <a class="thm-btn" href="#">BOOK NOW</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="tour">
+                            <a href="tour-place.html" class="tour-img">
+                                <img src="img/tours/tour-2.jpg" alt="tour-img">
+                                <p class="price">
+                                    <span>$1500</span>
+                                    <small>/ 3 Days</small>
+                                </p>
+                            </a>
+                            <div class="desc">
+                                <h4>Family Tour in Greece</h4>
+                                <p>Our development opt in to the projects they genuinely want to work on, committing wholeheartedly to delivering.</p>
+                                <div class="rating">
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star voted"></i>
+                                    <i class="fa fa-star"></i>
+                                    <i class="fa fa-star"></i>
+                                    <small>(145)</small>
+                                </div>
+                                <a class="thm-btn" href="#">BOOK NOW</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <!-- End single room section -->
+
+
+
+        <?php 
+       include("footer.php");
+       ?>
+		
+		<!-- all js files -->
+
+		<!-- jquery latest version -->
+		<script src="js/marco/jquery-1.12.4.min.js"></script>
+		<!-- bootstrap js -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- owl.carousel js -->
+		<script src="js/owl.carousel.min.js"></script>
+		<!-- Counter js -->
+		<script src="js/jquery.counterup.min.js"></script>
+		<!-- waypoint js -->
+		<script src="js/waypoints.js"></script>
+		<!-- isotope js -->
+        <script src="js/isotope.pkgd.min.js"></script>
+        <!-- stellar js -->
+        <script src="js/jquery.stellar.min.js"></script>
+        <!-- stellar js -->
+        <script src="js/imagelightbox.min.js"></script>
+		<!-- magnific js -->
+        <script src="js/magnific.min.js"></script>
+		<!-- venobox js -->
+		<script src="js/venobox.min.js"></script>
+        <!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+        <!-- tabs js -->
+        <script src="js/tabs.js"></script>
+		<!-- Form validator js -->
+		<script src="js/form-validator.min.js"></script>
+		<!-- plugins js -->
+		<script src="js/plugins.js"></script>
+        <!-- datapicker js -->
+        <script src="js/zebra_datepicker.min.js"></script>
+		<!-- main js -->
+		<script src="js/main.js"></script>
+	</body>
+</html>
