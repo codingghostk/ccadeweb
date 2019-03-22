abc731c175a57fb125c754db0f0ba5f34c0910bb
Kevin Flores <kevinflores@codingghost.com.mx>
2019-03-21 19:22:38 -0600

    kevin nuevo

--- index.php
+++ index.php
@@ -31,6 +31,10 @@
 		<link rel="stylesheet" href="css/responsive.css">
 		<!-- modernizr css -->
 		<script src="js/marco/modernizr-2.8.3.min.js"></script>
+
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/image-slider.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
         
 	</head>
 		<body>
@@ -59,7 +63,26 @@
             include("mainMenu.php");
         ?>
         <!-- MAin menu ends -->
-        <section class="slider-section">
+
+        <div id="slider-main">
+
+            <!-- previous button -->
+            <button id="prev"><i class="fa fa-angle-double-left"></i></button>
+
+            <!-- image container -->
+            <div id="slider">
+                
+            </div>
+
+            <!-- next button -->
+            <button id="next"><i class="fa fa-angle-double-right"></i></button>
+
+            <!-- small circles container -->
+            <div id="circles"></div>
+
+        </div>
+
+        <!--<section class="slider-section">
             <div class="container" style="padding: 0; margin: 0; width: 100%;">
               <div id="myCarousel" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators" style="display: none;">
@@ -111,7 +134,7 @@
             </a>
           </div>
         </div>
-        </section>
+        </section>-->
 
 
         <!-- Start Slider section -->
@@ -2731,5 +2754,24 @@
                
       }
        </script>
+
+       <!--jquery javascript library -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+<!-- image slider script -->
+<script src="js/image-slider.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 	</body>
 </html>
